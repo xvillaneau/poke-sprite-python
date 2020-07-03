bcdfd43c1a24941cf7fa844e8d7030e41eb55564
Xavier Villaneau <xvillaneau@gmail.com>
2020-07-03 14:23:11 -0400

    Add Prototype Decoding

--- pokemon_sprites/poke_rle.py
+++ pokemon_sprites/poke_rle.py
@@ -26,6 +26,13 @@ def decompress_sprite(bytes_stream, show=False):
         mode += read(1)
 
     _decompress_rle(bits, size, buffer_1)
+
+    if mode != 1:
+        delta_decode_buffer(buffer_1)
+    delta_decode_buffer(buffer_0)
+    if mode != 0:
+        buffer_1 ^= buffer_0
+
     if show:
         im = Image.frombytes("1", (56, 56), bytes(buffer_0))
         ImageShow.show(im)
@@ -60,3 +67,29 @@ def _decompress_rle(bit_stream, size, buffer):
         row = (written >> 1) % 56
         pos = 7 * row + (col // 4)
         shift = 6 - 2 * (col % 4)
+
+
+def delta_decode_buffer(buffer):
+    row, col, state = 0, 0, 0
+    while row < 56:
+        byte = buffer[row * 7 + col]
+
+        first = DELTA_DECODE_NIBBLE[byte >> 4] ^ (0b1111 * state)
+        state = first & 1
+        second = DELTA_DECODE_NIBBLE[byte & 0b1111] ^ (0b1111 * state)
+        state = second & 1
+
+        buffer[row * 7 + col] = (first << 4) + second
+        col += 1
+
+        if col >= 7:
+            row += 1
+            col, state = 0, 0
+
+
+DELTA_DECODE_NIBBLE = [
+    0b0000, 0b0001, 0b0011, 0b0010,  # 0000, 0001, 0010, 0011
+    0b0111, 0b0110, 0b0100, 0b0101,  # 0100, 0101, 0110, 0111
+    0b1111, 0b1110, 0b1100, 0b1101,  # 1000, 1001, 1010, 1011
+    0b1000, 0b1001, 0b1011, 0b1010,  # 1100, 1101, 1110, 1111
+]
