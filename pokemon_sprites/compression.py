78e0c5ca4301e27cb67a7ab32cbf012a59f47a69
Xavier Villaneau <xvillaneau@gmail.com>
2020-07-04 15:00:38 -0400

    Move decoding code into compression module
    Decoding is a decompression step after all, now the compression
    code will only be returning the data written to memory. And the
    detected size for convenience.

--- pokemon_sprites/compression.py
+++ pokemon_sprites/compression.py
@@ -27,7 +27,20 @@ def decompress_sprite(bytes_stream):
 
     decompress_to_buffer(bits, width, height, buffer_1)
 
-    return buffer_b, buffer_c, width, height, mode, invert_buffers
+    if mode == 1:
+        delta_decode_buffer(width, height, buffer_1)
+        delta_decode_buffer(width, height, buffer_0)
+
+    elif mode == 2:
+        delta_decode_buffer(width, height, buffer_0)
+        xor_buffers(width, height, buffer_1, buffer_0)
+
+    elif mode == 3:
+        delta_decode_buffer(width, height, buffer_1)
+        delta_decode_buffer(width, height, buffer_0)
+        xor_buffers(width, height, buffer_1, buffer_0)
+
+    return buffer_b, buffer_c, width, height
 
 
 def decompress_to_buffer(bit_stream, width, height, buffer):
@@ -72,3 +85,39 @@ def decompress_stream(bit_stream):
             for _ in range(rle_count):
                 yield 0
             mode = 1
+
+
+def delta_decode_buffer(width, height, buffer):
+    # Delta decoding is done by row, but data in the buffer is by
+    # columns of bytes from top to bottom. So we'll need to query
+    # a bit all around the place.
+
+    row, col, state = 0, 0, 0
+    while row < (height * 8):
+        pos = col * height * 8 + row
+        byte = buffer[pos]
+
+        first = DELTA_DECODE_NIBBLE[byte >> 4] ^ (0b1111 * state)
+        state = first & 1
+        second = DELTA_DECODE_NIBBLE[byte & 0b1111] ^ (0b1111 * state)
+        state = second & 1
+
+        buffer[pos] = (first << 4) + second
+        col += 1
+
+        if col >= width:
+            row += 1
+            col, state = 0, 0
+
+
+def xor_buffers(width, height, write_buffer, read_buffer):
+    for i in range(width * height * 8):
+        write_buffer[i] ^= read_buffer[i]
+
+
+DELTA_DECODE_NIBBLE = [
+    0b0000, 0b0001, 0b0011, 0b0010,  # 0000, 0001, 0010, 0011
+    0b0111, 0b0110, 0b0100, 0b0101,  # 0100, 0101, 0110, 0111
+    0b1111, 0b1110, 0b1100, 0b1101,  # 1000, 1001, 1010, 1011
+    0b1000, 0b1001, 0b1011, 0b1010,  # 1100, 1101, 1110, 1111
+]
--- pokemon_sprites/poke_rle.py
+++ pokemon_sprites/poke_rle.py
@@ -5,20 +5,8 @@ from .compression import decompress_sprite
 
 def read_sprite(bytes_stream, show=False):
 
-    buffer_b, buffer_c, width, height, mode, invert_buffers = decompress_sprite(bytes_stream)
+    buffer_b, buffer_c, width, height = decompress_sprite(bytes_stream)
     buffer_a = bytearray(392)
-
-    if invert_buffers:
-        buffer_0, buffer_1 = buffer_c, buffer_b
-    else:
-        buffer_0, buffer_1 = buffer_b, buffer_c
-
-    if mode != 2:
-        delta_decode_buffer(width, height, buffer_1)
-    delta_decode_buffer(width, height, buffer_0)
-    if mode != 1:
-        for i in range(width * height * 8):
-            buffer_1[i] ^= buffer_0[i]
 
     adjust_position(width, height, buffer_b, buffer_a)
     adjust_position(width, height, buffer_c, buffer_b)
@@ -26,37 +14,6 @@ def read_sprite(bytes_stream, show=False):
     if show:
         im = Image.frombuffer("L", (56, 56), render(buffer_a, buffer_b))
         ImageOps.invert(im).show()
-
-
-def delta_decode_buffer(width, height, buffer):
-    # Delta decoding is done by row, but data in the buffer is by
-    # columns of bytes from top to bottom. So we'll need to query
-    # a bit all around the place.
-
-    row, col, state = 0, 0, 0
-    while row < (height * 8):
-        pos = col * height * 8 + row
-        byte = buffer[pos]
-
-        first = DELTA_DECODE_NIBBLE[byte >> 4] ^ (0b1111 * state)
-        state = first & 1
-        second = DELTA_DECODE_NIBBLE[byte & 0b1111] ^ (0b1111 * state)
-        state = second & 1
-
-        buffer[pos] = (first << 4) + second
-        col += 1
-
-        if col >= width:
-            row += 1
-            col, state = 0, 0
-
-
-DELTA_DECODE_NIBBLE = [
-    0b0000, 0b0001, 0b0011, 0b0010,  # 0000, 0001, 0010, 0011
-    0b0111, 0b0110, 0b0100, 0b0101,  # 0100, 0101, 0110, 0111
-    0b1111, 0b1110, 0b1100, 0b1101,  # 1000, 1001, 1010, 1011
-    0b1000, 0b1001, 0b1011, 0b1010,  # 1100, 1101, 1110, 1111
-]
 
 
 def adjust_position(width, height, src_buffer, dest_buffer):
